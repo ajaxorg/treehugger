2c9b5d177601689fbb21c1443283a6d8846f126e
Zef Hemel <zef@zef.me>
2011-03-27 10:25:21 +0200

    Commit before traversal changes

--- scripts/parsejs.js
+++ scripts/parsejs.js
@@ -4,7 +4,6 @@ define(["ast", "narcissus/jsdefs", "narcissus/jslex", "narcissus/jsparse"], func
         var n = Narcissus.parser.parse(s);
         return transform(n);
       } catch(e) {
-        //console.log(e);
         return new ast.StringNode("Invalid Javascript: " + e.toString());
       }
     }
@@ -26,6 +25,8 @@ define(["ast", "narcissus/jsdefs", "narcissus/jslex", "narcissus/jsparse"], func
           }))], meta);
       case "return":
         return new ast.ConsNode("Return", [transform(n.value)], meta);
+      case "=":
+        return new ast.ConsNode("Assign", [transform(n.children[0]), transform(n.children[1])], meta);
       case "true":
         return new ast.ConsNode("True", [], meta);
       case "false":
@@ -40,8 +41,12 @@ define(["ast", "narcissus/jsdefs", "narcissus/jslex", "narcissus/jsparse"], func
         return new ast.ConsNode("ExpStat", [transform(n.expression)], meta);
       case "CALL":
         return new ast.ConsNode("Call", [transform(n.children[0]), new ast.ListNode(n.children[1].children.map(transform))], meta);
+      case "NEW_WITH_ARGS":
+        return new ast.ConsNode("New", [transform(n.children[0]), new ast.ListNode(n.children[1].children.map(transform))], meta);
+      case "OBJECT_INIT":
+        return new ast.ConsNode("ObjectInit", [transform(n.children[0]), new ast.ListNode(n.children[1].children.map(transform))], meta);
       case ".":
-        return new ast.ConsNode("FieldAccess", [transform(n.children[0]), transform(n.children[1])], meta);
+        return new ast.ConsNode("FieldAccess", [transform(n.children[0]), new ast.StringNode(n.children[1].value)], meta);
       case "IDENTIFIER":
         return new ast.ConsNode("Var", [new ast.StringNode(n.value)], meta);
       case "+":
@@ -52,6 +57,14 @@ define(["ast", "narcissus/jsdefs", "narcissus/jslex", "narcissus/jsparse"], func
         return new ast.ConsNode("Op", [new ast.StringNode("/"), transform(n.children[0]), transform(n.children[1])], meta);
       case "*":
         return new ast.ConsNode("Op", [new ast.StringNode("*"), transform(n.children[0]), transform(n.children[1])], meta);
+      case "||":
+        return new ast.ConsNode("Op", [new ast.StringNode("||"), transform(n.children[0]), transform(n.children[1])], meta);
+      case "&&":
+        return new ast.ConsNode("Op", [new ast.StringNode("&&"), transform(n.children[0]), transform(n.children[1])], meta);
+      case "!":
+        return new ast.ConsNode("Op", [new ast.StringNode("!"), transform(n.children[0])], meta);
+      case "REGEXP":
+        return new ast.ConsNode("RegExp", [new ast.StringNode(n.value.toString())], meta);
       default:
         console.log(n, Narcissus.definitions.tokens[n.type]);
       }
